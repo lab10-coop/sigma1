f9d4eafe3744be8ee550ceef18aa43102819cb37
didi <git@d10r.net>
2020-11-21 10:30:31 +0100

    added checksum check to migration script

--- migrate-db.sh
+++ migrate-db.sh
@@ -6,6 +6,7 @@ set -e
 set -u
 
 BIN=oe-upgrage-db-3-1
+CHECKSUM="62bf8b2346929a7f7bdbea31a20150196982b7104e598855f9de5435d1e586ac"
 DBDIR=data/chains/sigma1.artis/db/2fe13bcc0f05fba8/overlayrecent
 
 if [[ ! -d $DBDIR ]]; then
@@ -15,6 +16,12 @@ fi
 
 echo "downloading binary..."
 curl -s -f https://dev.lab10.io/artis/$BIN > $BIN
+file_checksum=$(sha256sum $BIN | cut -f 1 -d " ")
+if [[ $CHECKSUM != $file_checksum ]]; then
+	echo "checksum mismatch, aborting!"
+	exit 2
+fi
+
 chmod +x $BIN
 
 echo "running migration..."
